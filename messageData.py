4738b8c9e4d47a973c77e2da44fd4271a14efa26
Vin_04 <vinayakborhade@gmail.com>
2019-03-02 14:06:04 +0530

    modified messageData.py

--- messageData.py
+++ messageData.py
@@ -4,16 +4,17 @@ class messageData:
     BITMAP_DIM=784
     def __init__(self, folder='data/numpy_bitmap/'):
         self.folder = folder
-        self.X=np.zeros((0,784))
+        self.X=np.empty((3,784))
         self.Y=[]
-        for filename in os.listdir(self.folder):
+        for i,filename in enumerate(os.listdir(self.folder)):
             fullpath=self.folder+filename
             #use filename as Y
             name=filename[:-4]
-            x=np.load(fullpath)
-            y=[name]*x.shape[0]
+            x=np.load(fullpath,mmap_mode='r')
+            print("x.shape[0]: ",x.shape[0])
+            #y=[name]*x.shape[0]
             #print(self.X)
-            self.X=np.concatenate((self.X,x),axis=0)
+            self.X[i]=x
             #print(self.X)
             self.Y =self.Y + y
         self.Y=np.array(self.Y)
