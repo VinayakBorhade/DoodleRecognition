f6e3dd76bec6d0335836f1e1d4cc03efbaf0e339
Vin_04 <vinayakborhade@gmail.com>
2019-03-27 10:33:47 +0530

    modified basline.py - added timer

--- baseline.py
+++ baseline.py
@@ -1,22 +1,28 @@
 #baseline.py
 import numpy
 import pandas
+import time
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
+from keras.callbacks import TensorBoard
+
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
-from messageData import messageData
-#execfile('messageData.py')
-# TODO: make split (train, test, dev)
+
+#import massageData
+#import myConstants as mc
+
 CLASS_NUM = 3
-def baseline_model():
+BITMAP_DIM=784
+def baseline_models():
     # create model
     model = Sequential()
-    model.add(Dense(1, input_dim=messageData.BITMAP_DIM, activation='sigmoid'))
+    model.add(Dense(1, input_dim=BITMAP_DIM, activation='sigmoid'))
     model.add(Dense(CLASS_NUM, activation='softmax'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -24,15 +30,17 @@ def baseline_model():
 
 def main():
     print ("Done loading the libraries")
+    t0 = time.time()
     # fix random seed for reproducibility
     seed = 7
     numpy.random.seed(seed)
+
     # load dataset
     data = messageData()
-    X = data.getX()
-    Y = data.getY()
+    X, Y = data.getData()
+
     print ("Done load dataset")
-    #TODO: from 3 files, create the label and combine to 1 training data file
+
     # do some more preprocessing
     # encode class values as integers
     encoder = LabelEncoder()
@@ -40,12 +48,20 @@ def main():
     encoded_Y = encoder.transform(Y)
     # convert integers to dummy variables (i.e. one hot encoded)
     dummy_y = np_utils.to_categorical(encoded_Y)
+
     print ("Done preprocessing dataset")
+
     # build the model
-    estimator = KerasClassifier(build_fn=baseline_model, epochs=1, batch_size=500, verbose=1)
+    tensorboard = TensorBoard()
+    estimator = KerasClassifier(build_fn=baseline_models, epochs=10, batch_size=500, verbose=1)
+
     print ("Done building estimator")
+
     kfold = KFold(n_splits=2, shuffle=True, random_state=seed)
-    results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+
+    results = cross_val_score(estimator, X, dummy_y, cv=kfold, verbose=1, fit_params={'callbacks': [tensorboard]})
+    t1 = time.time()
+    print("Time elapsed: ", t1 - t0)
     print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
 if __name__ == '__main__':
