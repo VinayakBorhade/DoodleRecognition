307925226047e4740760f2341ff95b2557f43728
Vin_04 <vinayakborhade@gmail.com>
2019-03-30 09:28:22 +0530

    added generator and setLengths in messageData.py

--- messageData.py
+++ messageData.py
@@ -1,74 +1,64 @@
+#messageData.py modified
 import numpy as np
 import os
-#import myConstants as mc
-
-from sklearn.model_selection import train_test_split
+import random
 
 class messageData:
-  def getData(self):
-    """ Returns the full dataset. This is 100% of the loaded data. """
-    return (self.X, self.Y)
-
-  def getTrain(self):
-    """ Returns a tuple x,y for the training dataset. """
-    return (self.X_train, self.y_train)
-
-  def getTest(self):
-    """ Returns a tuple x,y for the test dataset. """
-    return (self.X_test, self.y_test)
-
-  def getDev(self):
-    """ Returns a tuple of x,y for the developmetn dataset. """
-    return (self.X_dev, self.y_dev)
-
-  def __init__(self, folder='data/numpy_bitmap/'):
-    self.BITMAP_DIM=784
-    self.folder = folder
-    self.X = np.zeros((0, self.BITMAP_DIM))
-    self.Y = []
-    self.X_train = np.zeros((0, self.BITMAP_DIM))
-    self.y_train = []
-    self.X_dev = np.zeros((0, self.BITMAP_DIM))
-    self.y_dev = []
-    self.X_test= np.zeros((0, self.BITMAP_DIM))
-    self.y_test = []
-    random_seed = 42
-    for filename in os.listdir(self.folder):
+  def __init(self):
+    self.total_len_train=0
+    self.total_len_valid=0
+  
+  def setLengths(self, folder='data/numpy_bitmap/'):
+    self.total_len_train=0
+    self.total_len_valid=0
+    self.folder=folder
+    flag=True
+    for i, filename in enumerate(os.listdir(self.folder)):
       fullpath = self.folder + filename
-      # use filename as Y label.
-      name = filename[:-4]
-      x = np.load(fullpath)
-      y = [name] * x.shape[0]
-      # Full dataset.
-      self.X = np.concatenate((self.X, x), axis=0)
-      self.Y += y
-      # Split into train/test/dev. See also comment above.
-      X_train, X_eval, y_train, y_eval = train_test_split(x, y, test_size=0.2,random_state=random_seed)
-      X_dev, X_test, y_dev, y_test = train_test_split(X_eval, y_eval, test_size=0.5, random_state=random_seed)
-      # Train split.
-      self.X_train = np.concatenate((self.X_train, X_train), axis=0)
-      self.y_train += y_train
-      # Dev split.
-      self.X_dev = np.concatenate((self.X_dev, X_dev), axis=0)
-      self.y_dev += y_dev
-      # Test split.
-      self.X_test = np.concatenate((self.X_test, X_test), axis=0)
-      self.y_test += y_test
+      name = filename.split('.')[0]
+      samples = np.load(fullpath, mmap_mode='r')
+      s_temp=int(samples.shape[0]*0.1)
+      self.total_len_train+= s_temp*0.7
+      self.total_len_valid+= s_temp*0.3
+      if flag:
+        flag=False
+        print("dimension of the image: ",samples[0].shape)
+        
+    self.total_len_train=int(self.total_len_train)
+    self.total_len_valid=int(self.total_len_valid)
+  
+  def generator(self, folder='data/numpy_bitmap/', isTrain=True, batch_size=32):
+    self.folder=folder
+    """
+    Yields the next training batch.
+    Suppose `samples` is an array [[image1_filename,label1], [image2_filename,label2],...].
+    samples is an array of 28*28 image [image1, image2,....]
+    """
+    while True:
+      for i, filename in enumerate(os.listdir(self.folder)):
+        fullpath = self.folder + filename
+        name = filename.split('.')[0]
+        samples_full = np.load(fullpath, mmap_mode='r')
+        samples_full_len = samples_full.shape[0]
+        samples = samples_full[0:int(0.1*samples_full_len)]
+        samples_len = samples.shape[0]
+        samples_local=np.array([])
+        start,end=0,0
+        if isTrain:
+          samples_local=samples[0:int(0.7*samples_len)]
+          start,end=0,int(0.7*samples_len)
+        else:
+          samples_local=samples[int(0.7*samples_len):samples_len]
+          start,end=0,int(0.3*samples_len)
 
-    # Convert into NP Arrays.
-    self.Y = np.array(self.Y)
-    self.y_train = np.array(self.y_train)
-    self.y_test = np.array(self.y_test)
-    self.y_dev = np.array(self.y_dev)
+        X,Y=np.array([]),[]
+        np.random.permutation(samples_local)
+        for offset in range(start, end, batch_size):
+          X=samples_local[offset:offset+batch_size]
+          Y=[i]*int(X.shape[0])
+          yield X, Y
 
-    print("All: ", self.X.shape)
-    print("train: ", self.X_train.shape)
-    print("Test: ", self.X_test.shape)
-    print("Dev: ", self.X_dev.shape)
-
-
-def main():
-	data = messageData()
-
-if __name__ == '__main__':
-	main()
+data=messageData()
+data.setLengths()
+print("total_len_train", data.total_len_train)
+print("total_len_valid", data.total_len_valid)
