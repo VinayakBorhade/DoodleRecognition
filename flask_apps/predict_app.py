15aa80ceea3266ceb2142c7f825f604bc17c60cc
Vin_04 <vinayakborhade@gmail.com>
2019-03-30 12:45:19 +0530

    started working on predict_app.py

---
+++ flask_apps/predict_app.py
@@ -0,0 +1,40 @@
+import base64
+import numpy as np
+import io
+from PIL import Image
+#import keras
+#from keras import backend as K
+import tensorflow as tf
+from keras.models import Sequential, load_model
+from keras.preprocessing.image import ImageDataGenerator, img_to_array
+from flask import request, jsonify, Flask
+app=Flask(__name__)
+def get_model():
+    global graph
+    graph = tf.get_default_graph()
+    global model
+    model=load_model("doodle_trial_model.h5")
+    print(" * Model loaded!")
+def preprocess_image(image, target_size):
+    image=image.resize(target_size)
+    image=img_to_array(image)
+    image=np.expand_dims(image, axis=0)
+    return image
+print(" * Loading Keras model...")
+get_model()
+@app.route("/predict", methods=["POST"])
+def predict():
+    message=request.get_json(force=True)
+    encoded=message['image']
+    decoded=base64.b64decode(encoded)
+    image=Image.open(io.BytesIO(decoded))
+    processed_image=preprocess_image(image, target_size=(784))
+    prediction=model.predict(processed_image).tolist()
+    response={
+        'prediction':{
+            'airplane':prediction[0][0],
+            'apple':prediction[0][1],
+            'banana':prediction[0][2]
+        }
+    }
+    return jsonify(response)
