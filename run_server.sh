df13c068162e38623f42c3bda780a957deb27785
Vinayak <Vin_04>
2019-04-13 11:31:10 +0530

    added bar graph in predict.html

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This is a script to run the Flask server with the deployed model
+chmod u+x run_server.sh
+chmod +w run_server.sh
 cd flask_apps
 export FLASK_APP=predict_app.py
 python -m flask run --host=0.0.0.0
